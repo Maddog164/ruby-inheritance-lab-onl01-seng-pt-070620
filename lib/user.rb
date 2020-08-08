a40cb9b05583cc30b9c806214501fb1092584a74
Denise Henderson <denise@henderson3.us>
2020-08-08 02:04:42 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,8 @@
 class User
 
-  attr_writer :first_name, :last_name
-  attr_reader :first_name, :last_name
+  attr_accessor :first_name, :last_name
 
-  def initialize
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last name
   end
